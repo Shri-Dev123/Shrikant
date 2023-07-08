d99ae7dc2f2413e8756794c17c3fe75f01485ffb
ShriDev-S7 <Shrikant@s7works.io>
2023-07-08 15:28:42 +0530

    (Fix):minor issue fix

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -6,13 +6,11 @@ import {
 } from "react-share";
 
 const ShareButton = ({ imageUrl }) => {
-    const shareUrl = window.location.href;
-
     return (
         <div className="flex flex-col md:flex-row items-center justify-center mt-4">
             <div className="flex flex-wrap gap-2.5 justify-center">
                 <FacebookShareButton
-                    url={shareUrl}
+                    url={imageUrl}
                     quote="Check out this image!"
                     imageUrl={imageUrl}
                 >
@@ -21,7 +19,7 @@ const ShareButton = ({ imageUrl }) => {
                     </button>
                 </FacebookShareButton>
                 <TwitterShareButton
-                    url={shareUrl}
+                    url={imageUrl}
                     hashtags={["randomImage"]}
                     image={imageUrl}
                 >
@@ -29,7 +27,7 @@ const ShareButton = ({ imageUrl }) => {
                         Share on Twitter
                     </button>
                 </TwitterShareButton>
-                <WhatsappShareButton url={shareUrl} image={imageUrl}>
+                <WhatsappShareButton url={imageUrl} image={imageUrl}>
                     <button className="share-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mb-4 px-4 rounded">
                         Share on WhatsApp
                     </button>
